de94e7325cbef42c0da79f2ea43c890cf352d8c8
chevere <vitaly@batushev.info>
2021-01-11 14:35:00 +0300

    first commit

---
+++ config.go
@@ -0,0 +1,152 @@
+package main
+
+type config struct {
+	Info             string       `json:"info"`
+	Abbrevation      abbrevation  `json:"abbrevation"`
+	Addional         addional     `json:"addional"`
+	Ages             bool         `json:"ages"`
+	Augment          bool         `json:"augment"`
+	Currency         currency     `json:"currency"`
+	Dash             dash         `json:"dash"`
+	Digital          digitales    `json:"digital"`
+	DigitalPlace     digitalPlace `json:"digital_place"`
+	Ellipsis         bool         `json:"ellipsis"`
+	French           french       `json:"french"`
+	Initials         initials     `json:"initials"`
+	LastWord         bool         `json:"last_word"`
+	Measurements     measurements `json:"measurements"`
+	NoBreak          bool         `json:"nobreak"`
+	Monosyllabic     monosyllabic `json:"monosyllabic"`
+	Particle         particle     `json:"particle"`
+	Phone            bool         `json:"phone"`
+	PercentHairSpace bool         `json:"percent_hair_space"`
+	Quotes           quotes       `json:"quotes"`
+	Removes          removes      `json:"removes"`
+	Slashes          slashes      `json:"slashes"`
+	Square           bool         `json:"square"`
+	WebNoBreak       bool         `json:"web_nobreak"`
+	Rules            []rule       `json:"rules"`
+	// ChkOut           bool         `json:"chk_out"`
+}
+
+type enabled struct {
+	Enable bool `json:"enable"`
+}
+
+type spaced struct {
+	Space uint `json:"space"`
+}
+
+type listed struct {
+	List []string `json:"list"`
+}
+
+type regexped struct {
+	Regexp bool `json:"regexp"`
+}
+
+type abbrevation struct {
+	enabled
+	listed
+	spaced
+}
+
+type addional struct {
+	enabled
+	Currencies          []string `json:"currencies"`
+	NoBreakHyphenAfter  []string `json:"no_break_hyphen_after"`
+	NoBreakHyphenBefore []string `json:"no_break_hyphen_before"`
+	ForMinusSign        []string `json:"minus_signes"`
+}
+
+type slashes struct {
+	enabled
+	HairSpace bool `json:"hair_space"`
+}
+
+type removes struct {
+	DoubleParagraphs bool `json:"double_paragraphs"`
+	DoubleSpaces     bool `json:"double_spaces"`
+	TabFirst         bool `json:"tab_first"`
+	TabLast          bool `json:"tab_last"`
+}
+
+type quotes struct {
+	enabled
+	QuoteType uint `json:"type"`
+}
+
+type monosyllabic struct {
+	enabled
+	listed
+	regexped
+	CaseSens bool `json:"casesens"`
+}
+
+type measurements struct {
+	Abbrevation bool `json:"abbrevation"`
+	listed
+	spaced
+}
+
+type initials struct {
+	enabled
+	Before  uint `json:"before"`
+	Between uint `json:"between"`
+}
+
+type digitalPlace struct {
+	enabled
+	spaced
+	Exists bool `json:"exists"`
+}
+
+type dash struct {
+	enabled
+	BeginSpace         uint `json:"begin_space"`
+	SpaceAfterPunct    bool `json:"space_after_punct"`
+	Em                 bool `json:"em"`
+	En                 bool `json:"en"`
+	SpaceAfter         uint `json:"space_after"`
+	SpaceAfterNoBreak  bool `json:"space_after_no_break"`
+	SpaceAfterWidth    uint `json:"space_after_width"`
+	SpaceBefore        uint `json:"space_before"`
+	SpaceBeforeNoBreak bool `json:"space_before_no_break"`
+	SpaceBeforeWidth   uint `json:"space_before_width"`
+}
+
+type french struct {
+	enabled
+	spaced
+}
+
+type particle struct {
+	enabled
+	listed
+	regexped
+}
+
+type currency struct {
+	enabled
+	spaced
+}
+
+type digitales struct {
+	Arabes    bool `json:"arabes"`
+	Latin     bool `json:"latin"`
+	Dash      uint `json:"dash"`
+	DashSpace uint `json:"dash_space"`
+	Minus     bool `json:"minus"`
+}
+
+type rule struct {
+	enabled
+	Name         string `json:"name"`
+	Find         string `json:"find"`
+	FindPref     string `json:"find_pref"`
+	Change       string `json:"change"`
+	ChangePref   string `json:"change_pref"`
+	Footnote     bool   `json:"footnote"`
+	MasterPage   bool   `json:"masters"`
+	HiddenLayers bool   `json:"hiddens"`
+}
---
+++ szam5cnv.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	appName = "sZam5 config converter"
+	appVer  = "1.0.0"
+)
+
+// Set - ...
+type Set struct {
+	XMLName                xml.Name `xml:"Sets"`
+	Ages                   bool     `xml:"setAges"`
+	ArabDigital            bool     `xml:"setArabDigital"`
+	Chast                  bool     `xml:"setChast"`
+	ChastList              []string `xml:"setChastList"`
+	ChastRegEx             bool     `xml:"setChastRegEx"`
+	Currency               bool     `xml:"setCurrency"`
+	CurrencySpace          uint     `xml:"setCurrencySpace"`
+	Dash                   bool     `xml:"setDash"`
+	DashBeginSpace         uint     `xml:"setDashBeginSpace"`
+	DashSpaceAfterPunct    bool     `xml:"setDashSpaceAfterPunct"`
+	DashEm                 bool     `xml:"setDashEm"`
+	DashEn                 bool     `xml:"setDashEn"`
+	DashSpaceAfter         uint     `xml:"setDashSpaceAfter"`
+	DashSpaceAfterNoBreak  bool     `xml:"setDashSpaceAfterNoBreak"`
+	DashSpaceAfterWidth    uint     `xml:"setDashSpaceAfterWidth"`
+	DashSpaceBefore        uint     `xml:"setDashSpaceBefore"`
+	DashSpaceBeforeNoBreak bool     `xml:"setDashSpaceBeforeNoBreak"`
+	DashSpaceBeforeWidth   uint     `xml:"setDashSpaceBeforeWidth"`
+	DigitalDash            uint     `xml:"setDigitalDash"`
+	DigitalDashSpace       uint     `xml:"setDigitalDashSpace"`
+	DigitalMinus           bool     `xml:"setDigitalMinus"`
+	FrenchEnable           bool     `xml:"setFrenchEnable"`
+	FrenchSpace            uint     `xml:"setFrenchSpace"`
+	ExistRazriad           bool     `xml:"setExistRazriad"`
+	Info                   string   `xml:"setInfo"`
+	Initials               bool     `xml:"setInitials"`
+	InitialsBefore         uint     `xml:"setInitialsBefore"`
+	InitialsBetween        uint     `xml:"setInitialsBetween"`
+	LastWord               bool     `xml:"setLastWord"`
+	LatinDigital           bool     `xml:"setLatinDigital"`
+	MesSocr                bool     `xml:"setMesSocr"`
+	MesSocrList            []string `xml:"setMesSocrList"`
+	MesSocrSpace           uint     `xml:"setMesSocrSpace"`
+	Ellipsis               bool     `xml:"setMultidots"`
+	Augment                bool     `xml:"setNar"`
+	NoBreak                bool     `xml:"setNoBreak"`
+	Monosyllabic           bool     `xml:"setOneWord"`
+	MonosyllabicCaseSens   bool     `xml:"setOneWordCaseSens"`
+	MonosyllabicList       []string `xml:"setOneWordList"`
+	MonosyllabicRegEx      bool     `xml:"setOneWordRegEx"`
+	Phone                  bool     `xml:"setPhone"`
+	PercentHairSpace       bool     `xml:"setProcentHairSpace"`
+	Square                 bool     `xml:"setQuadrat"`
+	Quotes                 bool     `xml:"setQuotes"`
+	QuotesType             uint     `xml:"setQuotesType"`
+	Razriad                bool     `xml:"setRazriad"`
+	RazriadSpace           uint     `xml:"setRazriadSpace"`
+	RemoveDoublePara       bool     `xml:"setRemoveDoublePara"`
+	RemoveDoubleSpaces     bool     `xml:"setRemoveDoubleSpaces"`
+	RemoveFirstTab         bool     `xml:"setRemoveFirstTab"`
+	RemoveLastTab          bool     `xml:"setRemoveLastTab"`
+	Slashes                bool     `xml:"setSlashes"`
+	SlashesHairSpace       bool     `xml:"setSlashesHairSpace"`
+	Socr                   bool     `xml:"setSocr"`
+	SocrList               []string `xml:"setSocrList"`
+	SocrSpace              uint     `xml:"setSocrSpace"`
+	WebNoBreak             bool     `xml:"setWebNoBreak"`
+	ChkOut                 bool     `xml:"setChkOut"`
+	UseAddional            bool     `xml:"setUseAddional"`
+	AdCurrencies           []string `xml:"setAdCurrencies"`
+	AdYears                []string `xml:"setAdYears"`
+	AdNoBreakHyppenBefore  []string `xml:"setAdNoBreakHyppenBefore"`
+	AdNoBreakHyppenAfter   []string `xml:"setAdNoBreakHyppenAfter"`
+	AdForMinusSigns        []string `xml:"setAdForMinusSigns"`
+	Rule                   []Rule
+}
+
+// Rule - ...
+type Rule struct {
+	XMLName      xml.Name `xml:"Rule"`
+	Name         string   `xml:"name,attr"`
+	Enable       bool     `xml:"enable,attr"`
+	Find         string
+	FindPref     string
+	Change       string
+	ChangePref   string
+	Footnote     bool
+	MasterPage   bool
+	HiddenLayers bool
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(appName, appVer)
+		return
+	}
+
+	inFile := os.Args[1]
+	if f, err := os.Stat(inFile); err != nil || !strings.HasSuffix(f.Name(), ".xn") {
+		fmt.Println(appName, appVer)
+		log.Println("Файл", inFile, "не существует или не является файлом .XN")
+		return
+	}
+
+	var set Set
+	bytes, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err = xml.Unmarshal(bytes, &set); err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = buildConfig(inFile, set)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func buildConfig(f string, set Set) error {
+	var cfg config
+	cfg.Info = set.Info
+
+	cfg.Abbrevation.Enable = set.Socr
+	cfg.Abbrevation.List = set.SocrList
+	cfg.Abbrevation.Space = set.SocrSpace
+
+	cfg.Addional.Enable = set.UseAddional
+	cfg.Addional.Currencies = set.AdCurrencies
+	cfg.Addional.ForMinusSign = set.AdForMinusSigns
+	cfg.Addional.NoBreakHyphenAfter = set.AdNoBreakHyppenAfter
+	cfg.Addional.NoBreakHyphenBefore = set.AdNoBreakHyppenBefore
+
+	cfg.Ages = set.Ages
+
+	cfg.Augment = set.Augment
+
+	cfg.Currency.Enable = set.Currency
+	cfg.Currency.Space = set.CurrencySpace
+
+	cfg.Dash.Enable = set.Dash
+	cfg.Dash.BeginSpace = set.DashBeginSpace
+	cfg.Dash.SpaceAfter = set.DashSpaceAfter
+	cfg.Dash.SpaceAfterNoBreak = set.DashSpaceAfterNoBreak
+	cfg.Dash.SpaceAfterPunct = set.DashSpaceAfterPunct
+	cfg.Dash.SpaceAfterWidth = set.DashSpaceAfterWidth
+	cfg.Dash.SpaceBefore = set.DashSpaceBefore
+	cfg.Dash.SpaceBeforeNoBreak = set.DashSpaceBeforeNoBreak
+	cfg.Dash.SpaceBeforeWidth = set.DashSpaceBeforeWidth
+
+	cfg.Digital.Arabes = set.ArabDigital
+	cfg.Digital.Latin = set.LatinDigital
+	cfg.Digital.Dash = set.DigitalDash
+	cfg.Digital.DashSpace = set.DigitalDashSpace
+	cfg.Digital.Minus = set.DigitalMinus
+
+	// Запись JSON
+	result, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(strings.TrimSuffix(f, ".xn")+".json", result, os.ModePerm)
+}
